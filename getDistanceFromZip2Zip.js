617b6cca5d60313e5e569639249f9ec42a4f9481
Andre Bradshaw <bradshaw.andre@gmail.com>
2019-04-14 12:10:22 -0400

    Update getDistanceFromZip2Zip.js

--- getDistanceFromZip2Zip.js
+++ getDistanceFromZip2Zip.js
@@ -1,8 +1,5 @@
 var targetZipLatLng = (zp,arr) => arr.filter(z=> z.zip == zp)[0];
 
-var start = targetZipLatLng("35080", geodata);
-var end = targetZipLatLng("35143", geodata);
-
 function dist(targ1,targ2) {
   var lat1 = targ1.latitude, lng1 = targ1.longitude, 
     lat2 = targ2.latitude, lng2 = targ2.longitude;
@@ -12,7 +9,10 @@ function dist(targ1,targ2) {
     c(lat1 * p) * c(lat2 * p) *
     (1 - c((lng2 - lng1) * p)) / 2;
   return (0.621371 * (12742 * Math.asin(Math.sqrt(a))) * 1.25); 
-  /*mile conversion (km dist by crow fly) road apprx */
+  /*mile conversion * (km dist by crow fly) * road travel apprx */
 }
 
-console.log((dist(start,end))*1.25);
+var start = targetZipLatLng("35080", geodata);
+var end = targetZipLatLng("35143", geodata);
+var distance = dist(start,end);
+console.log(distance)
